5c921b3c17c7eae39a3f8532c62b9e83d7ea77ea
“dongoctoan916” <dongoctoan916@gmail.com>
2025-02-28 16:18:43 +0700

    BT 13/01

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d950e6edcee11e107c69d2d447d2c2395d93fc3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eacf2b734c8b85e8605f82d33aed4406c3e3ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
